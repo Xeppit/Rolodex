167b47c0f076eb008d85d0ee3c28f933e0ebf49c
Nathan Wilson <Nathan@Xeppit.co.uk>
2020-07-06 17:56:05 +0100

    Add project files.

---
+++ Rolodex.API/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rolodex.API.Models
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ Rolodex.API/Models/AddressContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rolodex.API.Models
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
